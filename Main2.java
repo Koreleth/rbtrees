68e84d626c8e9309f8e1be0ae79cd9f6beeff0fc
Koreleth <korel3@web.de>
2025-01-20 13:00:18 +0100

    Alles

---
+++ Main2.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+
+public class Main2 {
+    public static void main(String[] args) {
+        RBTree2<IntComparable> tree = new RBTree2<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 15; i++) {
+            int randomValue = random.nextInt(100); // Zufällige Zahl zwischen 0 und 99
+            IntComparable intComparable = new IntComparable(randomValue);
+            System.out.println("Inserting: " + intComparable);
+            tree.insert(intComparable);
+
+            // DOT-Datei nach jedem Schritt speichern
+            String filename = "tree_step_" + i + ".dot";
+            tree.printDOT(filename);
+            System.out.println("DOT file written: " + filename);
+        }
+    }
+    
+    public static class IntComparable implements Comparable<IntComparable> {
+        private int value;
+
+        public IntComparable(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        @Override
+        public int compareTo(IntComparable other) {
+            return Integer.compare(this.value, other.value);
+        }
+
+        @Override
+        public String toString() {
+            return Integer.toString(value);
+        }
+    }
+}
